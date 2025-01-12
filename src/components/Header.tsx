5319ae0876b55b387020daee86c53934e8d9e02f
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-01-12 23:48:12 +0300

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+import user from "../assets/user.png"
+
+export default function Header() {
+    return <header className="header">
+        <Link className="logo" to="/">#ADVENTURE VANS</Link>
+        <nav className="navbar">
+            <NavLink
+                to="/host"
+                className={({isActive}) => isActive ? "active-link": ""}
+            >
+                Host
+            </NavLink>
+            <NavLink
+                to="/about"
+                className={({isActive}) => isActive ? "active-link": ""}
+            >
+                About
+            </NavLink>
+            <NavLink
+                to="/vans"
+                className={({isActive}) => isActive ? "active-link": ""}
+            >
+                Vans
+            </NavLink>
+            <NavLink
+                to="/profile"
+                className={({isActive}) => isActive ? "active-link": ""}
+            >
+                <img src={user} alt="profile"></img>
+            </NavLink>
+        </nav>
+    </header>
+}
