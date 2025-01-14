b4253c5ca441c3f6e5bf566606e50dea927a12cd
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-01-14 14:49:51 +0300

    Fix styling and link issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,6 @@ export default function Home() {
     return <div className="home-page">
         <h1>You got the travel plans, we got the travel vans</h1>
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        <Link to='/van' className="find-van-btn">Find your van</Link>
+        <Link to='/vans' className="find-van-btn">Find your van</Link>
     </div>
 }
