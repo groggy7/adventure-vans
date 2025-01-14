219b7efec2e9d80c52bf3dca3046daa7a0d42a1c
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-01-15 00:12:07 +0300

    Add van filters

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 interface Van {
     id: number
     name: string
@@ -12,6 +13,7 @@ interface Van {
 
 export default function Vans() {
     const [vans, setVans] = React.useState<Van[] | null>(null)
+    const [searchParams, setSearchParams] = useSearchParams()
 
     React.useEffect(() => {
         fetch('/api/vans')
@@ -19,20 +21,61 @@ export default function Vans() {
         .then(data => setVans(data.vans))
     }, [])
 
-    const vanCards = vans?.map(van => {
-        return <div className="van-card">
+    const typeFilter = searchParams.get('type')
+    const displayedVans = typeFilter ? vans?.filter(van => van.type === typeFilter) : vans
+
+    const vanCards = displayedVans?.map(van => {
+        return <Link to={`/van/${van.id}`} className="van-card">
             <img src={van.imageUrl} alt={van.description} className="van-image" />
             <div className="van-info">
                 <span>{van.name}</span>
                 <span>${van.price}/day</span>
             </div>
             <p className={`van-type ${van.type}`}>{van.type}</p>
-        </div>
+        </Link>
     })
+
+    function handleFilterChange(type: string, filter: string) {
+        setSearchParams(prevParams => {
+            if(filter) {
+                prevParams.set(type, filter)
+            } else {
+                prevParams.delete(type)
+            }
+            return prevParams
+        })
+    }
 
     return vans ? 
     <div className="van-page">
-        {vanCards}
+        <div className="van-page-header-container">
+            <h1 className="van-page-header">Explore our van options</h1>
+            <div className="van-filter-btns">
+                <button 
+                    className={`van-filter-btn ${typeFilter==='simple' ? 'selected-simple' : ''}`}
+                    onClick={() => handleFilterChange('type', 'simple')}
+                >Simple
+                </button>
+                <button 
+                    className={`van-filter-btn ${typeFilter==='luxury' ? 'selected-luxury' : ''}`}
+                    onClick={() => handleFilterChange('type', 'luxury')}>
+                Luxury
+                </button>
+                <button 
+                    className={`van-filter-btn ${typeFilter==='rugged' ? 'selected-rugged' : ''}`}
+                    onClick={() => handleFilterChange('type', 'rugged')}>
+                Rugged
+                </button>
+                <button 
+                    className="van-filter-btn clear"
+                    onClick={() => handleFilterChange('type', '')}>
+                Clear filters
+                </button>
+            </div>
+        </div>
+        <div className="van-cards">
+            {vanCards}
+        </div>
     </div> 
     : <h1>Loading...</h1>
 }
