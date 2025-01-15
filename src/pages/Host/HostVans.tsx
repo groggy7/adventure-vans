4843bdb47bca66c57a4fc2beb574adb3c6a1819e
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-01-15 22:47:49 +0300

    Add host vans page

--- src/pages/Host/HostVans.tsx
+++ src/pages/Host/HostVans.tsx
@@ -1,3 +1,44 @@
+import React from "react"
+
+interface Van {
+    id: number
+    name: string
+    price: number
+    description: string
+    imageUrl: string
+    hostId: number
+    type: string
+}
+
 export default function HostVans() {
-    return <h1>Vans page goes here</h1>
+    const [vans, setVans] = React.useState<Van[] | null>(null)
+    
+    React.useEffect(() => {
+        fetch('/api/host/vans')
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    const vanCards = vans?.map(van => {
+        return <div className="host-van-card" key={van.id}>
+            <img src={van.imageUrl} alt={van.description} />
+            <div className="host-van-card-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}/day</p>
+            </div>
+            <button className="edit-btn right-aligned">Edit</button>
+        </div>
+    })
+
+    return <div className="dashboard-listed-vans">
+        <div>
+            <div className="dashboard-listed-vans-header">
+                <h2 className="host-van-page larger-text">Your listed vans</h2>
+                <button>View all</button>
+            </div>
+            <div className="host-van-cards">
+                {vanCards}
+            </div>
+        </div>
+    </div>
 }
